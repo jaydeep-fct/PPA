7a32c7d0dfd429794863cacd8a1d814a5c798bca
Jaydeep Popat Godase <jaydeep.godase@fourcolorstech.com>
2023-03-15 00:31:20 +0530

    Clearing the concept C++ programming the topic of this

--- Learning-C++/this.cpp
+++ Learning-C++/this.cpp
@@ -12,10 +12,28 @@ class Demo
         No1 = i;
         No2 = j;
     }
+
+    void Fun(int A, int B) // void Fun(Demo *this, int A, int B)
+    {
+        cout<<"inside the Fun :"<<this->No1<<"\n";
+        cout<<"inside the Fun :"<<this->No2<<"\n";
+    }
+    void Gun(int A)// void Gun(Demo *this, int A)
+    {
+        cout<<"inside the Gun :"<<this->No1<<"\n";
+        cout<<"inside the Gun :"<<this->No2<<"\n";
+    }
     
 };
 
 int main()
 {
+    Demo obj1(9,18);
+    Demo obj2(18,9);
+
+    obj1.Fun(1,8);  //Fun(&obj1,1,8) fun(100,1,8)
+    obj2.Gun(44); //Gun(&obj2,44) gun(200,44)
     return 0;
 }
+
+// Every C++ program gets internally converted into C program
